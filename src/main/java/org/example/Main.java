069d9f05bdab93a373a1e0841f4628ec80627893
jarne-besjes <91670760+jarne-besjes@users.noreply.github.com>
2024-11-07 13:46:53 +0100

    Add gen_csv mode

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvException;
 import net.sourceforge.argparse4j.impl.Arguments;
 import org.apache.lucene.analysis.Analyzer;
@@ -47,22 +48,11 @@ public class Main {
     }
 
     private static void add_arguments_to_parser(ArgumentParser parser) {
-        parser.addArgument("--query", "-q")
-                .help("Query to search for")
-                .type(String.class);
-        parser.addArgument("-n")
-                .help("Number of results to return")
-                .type(Integer.class);
-        parser.addArgument("--no-index")
-                .help("Do not index the documents")
-                .action(Arguments.storeTrue());
-        parser.addArgument("--docs-folder")
-                .help("Folder containing the documents to index")
-                .type(String.class);
-        parser.addArgument("--mode", "-m")
-                .help("Mode to run the program in")
-                .choices("query", "bench", "gen_csv")
-                .setDefault("query");
+        parser.addArgument("--query", "-q").help("Query to search for").type(String.class);
+        parser.addArgument("-n").help("Number of results to return").type(Integer.class);
+        parser.addArgument("--no-index").help("Do not index the documents").action(Arguments.storeTrue());
+        parser.addArgument("--docs-folder").help("Folder containing the documents to index").type(String.class);
+        parser.addArgument("--mode", "-m").help("Mode to run the program in").choices("query", "bench", "gen_csv").setDefault("query");
     }
 
     static void generateIndex(Path indexPath, Path corpusPath, Analyzer analyzer) throws IOException {
@@ -71,24 +61,21 @@ public class Main {
         writerConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         try (var indexWriter = new IndexWriter(indexDir, writerConfig)) {
             try (Stream<Path> paths = Files.walk(corpusPath)) {
-                paths
-                        .filter(Files::isRegularFile)
-                        .filter(path -> path.toString().endsWith(".txt"))
-                        .forEach(path -> {
-                            try {
-                                var content = Files.readString(path);
-                                var fileName = path.getFileName().toString();
-                                var docIdStr = fileName.substring(fileName.indexOf("_") + 1, fileName.indexOf("."));
-                                var docId = Integer.parseInt(docIdStr);
-                                var doc = new Document();
-                                doc.add(new TextField("title", fileName, Field.Store.YES));
-                                doc.add(new IntField("docId", docId, Field.Store.YES));
-                                doc.add(new TextField("content", content, Field.Store.NO));
-                                indexWriter.addDocument(doc);
-                            } catch (IOException e) {
-                                System.err.println("Error reading file " + path + ": " + e.getMessage());
-                            }
-                        });
+                paths.filter(Files::isRegularFile).filter(path -> path.toString().endsWith(".txt")).forEach(path -> {
+                    try {
+                        var content = Files.readString(path);
+                        var fileName = path.getFileName().toString();
+                        var docIdStr = fileName.substring(fileName.indexOf("_") + 1, fileName.indexOf("."));
+                        var docId = Integer.parseInt(docIdStr);
+                        var doc = new Document();
+                        doc.add(new TextField("title", fileName, Field.Store.YES));
+                        doc.add(new IntField("docId", docId, Field.Store.YES));
+                        doc.add(new TextField("content", content, Field.Store.NO));
+                        indexWriter.addDocument(doc);
+                    } catch (IOException e) {
+                        System.err.println("Error reading file " + path + ": " + e.getMessage());
+                    }
+                });
             }
         }
     }
@@ -97,10 +84,7 @@ public class Main {
         var queryStr = "does xpress bet charge to deposit money in your account";
         var query = new QueryParser("content", analyzer).parse(queryStr);
 
-        var indexDir = FSDirectory.open(indexPath);
-
-        var indexReader = DirectoryReader.open(indexDir);
-        var searcher = new IndexSearcher(indexReader);
+        var searcher = getIndexSearcher(indexPath);
         var topDocs = searcher.search(query, 10);
         ScoreDoc[] hits = topDocs.scoreDocs;
         var storedFields = searcher.storedFields();
@@ -113,10 +97,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        ArgumentParser parser = ArgumentParsers.newFor("Main").build()
-                .usage("Main [OPTIONS]")
-                .defaultHelp(true)
-                .description("Index and search documents");
+        ArgumentParser parser = ArgumentParsers.newFor("Main").build().usage("Main [OPTIONS]").defaultHelp(true).description("Index and search documents");
 
         add_arguments_to_parser(parser);
 
@@ -138,19 +119,53 @@ public class Main {
         } else if (res.get("mode").equals("bench")) {
             runBenchmark(indexPath, getAnalyzer());
         } else if (res.get("mode").equals("gen_csv")) {
-            // generate_csv(indexPath, getAnalyzer());
+            generate_csv(indexPath, getAnalyzer());
         }
     }
 
+    private static void write_csv(List<int[]> results, FileWriter writer) {
+        try {
+            writer.write("Query_nr,doc_nr\n");
+            for (var result : results) {
+                writer.append(result[0] + "," + result[1] + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    private static void generate_csv(Path indexPath, Analyzer analyzer) throws IOException, CsvException, ParseException {
+        var queries = getQueries();
+        var searcher = getIndexSearcher(indexPath);
+
+        var csv_results = new ArrayList<int[]>();
+
+        for (var query : queries) {
+            var query_nr = query[0];
+            var query_str = QueryParser.escape(query[1]);
+
+            var query_obj = new QueryParser("content", analyzer).parse(query_str);
+            var topDocs = searcher.search(query_obj, 10);
+            var doc_titles = getDocTitles(topDocs, searcher);
+
+            var docNrs = Arrays.stream(doc_titles).map(title -> title.substring(title.indexOf("_") + 1, title.indexOf("."))).toArray(String[]::new);
+
+            for (var docNr : docNrs) {
+                csv_results.add(new int[]{Integer.parseInt(query_nr), Integer.parseInt(docNr)});
+            }
+        }
+
+        var writer = new FileWriter("results.csv");
+        write_csv(csv_results, writer);
+    }
+
     private static void runBenchmark(Path indexPath, Analyzer analyzer) throws IOException, CsvException, ParseException {
-        var queriesReader = new CSVReaderBuilder(new FileReader("queries.tsv")).withSkipLines(1).withCSVParser(new CSVParserBuilder().withSeparator('\t').build()).build();
-        var queries = queriesReader.readAll();
+        var queries = getQueries();
         var resultsReader = new CSVReaderBuilder(new FileReader("expected_results.csv")).withSkipLines(1).withCSVParser(new CSVParserBuilder().withSeparator(',').build()).build();
         var expectedResults = resultsReader.readAll();
 
-        var indexDir = FSDirectory.open(indexPath);
-        var indexReader = DirectoryReader.open(indexDir);
-        var searcher = new IndexSearcher(indexReader);
+        var searcher = getIndexSearcher(indexPath);
 
         List<List<Float>> avgPrecisions = new ArrayList<>();
         List<List<Float>> avgRecalls = new ArrayList<>();
@@ -168,13 +183,9 @@ public class Main {
 
             var docTitles = getDocTitles(topDocs, searcher);
 
-            var docNrs = Arrays.stream(docTitles)
-                    .map(title -> title.substring(title.indexOf("_") + 1, title.indexOf(".")))
-                    .toArray(String[]::new);
+            var docNrs = Arrays.stream(docTitles).map(title -> title.substring(title.indexOf("_") + 1, title.indexOf("."))).toArray(String[]::new);
 
-            var expected = expectedResults.stream()
-                    .filter(r -> r[0].equals(queryNr))
-                    .map(row -> row[1]).toList();
+            var expected = expectedResults.stream().filter(r -> r[0].equals(queryNr)).map(row -> row[1]).toList();
 
             int[] ks = {1, 3, 5, 10};
             for (int ki = 0; ki < ks.length; ki++) {
@@ -187,17 +198,33 @@ public class Main {
             }
         }
 
-        var meanAvgPrecisions = avgPrecisions.stream()
-                .map(Main::mean)
-                .toList();
-        var meanAvgRecalls = avgRecalls.stream()
-                .map(Main::mean)
-                .toList();
+        var meanAvgPrecisions = avgPrecisions.stream().map(Main::mean).toList();
+        var meanAvgRecalls = avgRecalls.stream().map(Main::mean).toList();
 
-        System.out.println("k,mean_avg_precision,mean_avg_recall");
+        System.out.println("k | mean_avg_precision | mean_avg_recall");
         for (int i = 0; i < 4; i++) {
-            System.out.println((i + 1) + "," + meanAvgPrecisions.get(i) + "," + meanAvgRecalls.get(i));
+            int k = -1;
+            switch (i) {
+                case 0 -> k = 1;
+                case 1 -> k = 3;
+                case 2 -> k = 5;
+                case 3 -> k = 10;
+            }
+            System.out.println(k + " | " + meanAvgPrecisions.get(i) + " | " + meanAvgRecalls.get(i));
         }
+    }
+
+    private static IndexSearcher getIndexSearcher(Path indexPath) throws IOException {
+        var indexDir = FSDirectory.open(indexPath);
+        var indexReader = DirectoryReader.open(indexDir);
+        var searcher = new IndexSearcher(indexReader);
+        return searcher;
+    }
+
+    private static List<String[]> getQueries() throws IOException, CsvException {
+        var queriesReader = new CSVReaderBuilder(new FileReader("queries.tsv")).withSkipLines(1).withCSVParser(new CSVParserBuilder().withSeparator('\t').build()).build();
+        var queries = queriesReader.readAll();
+        return queries;
     }
 
     private static double mean(List<Float> list) {
